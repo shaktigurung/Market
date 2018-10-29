179cf00cf4706eccc535c3045ded91bc90d0d2e3
Daniel Teale <djteale90@gmail.com>
2018-10-29 14:13:57 +1100

    Functioning image upload and display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+  enum condition: {unused: 1, used: 2}
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fill: [50,50]
+  end
   #
   # def scale(width, height)
   #   # do something

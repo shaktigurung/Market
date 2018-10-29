b2ae2fbf8f424d9bcf7778f4b76ac764c6fe09ce
Daniel Teale <djteale90@gmail.com>
2018-10-29 16:43:00 +1100

    Implemented Multiple Product Images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+    resources :product_images
+  end
   devise_for :users
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

77f50df69da874012816753c894b60629137d3ef
shaktigurung <mr.shakti.gurung@gmail.com>
2018-10-30 16:37:00 +1100

    Stripe with Devise fused

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create_commit :add_customer_id
+
+  def add_customer_id
+    if self.customer_id.nil?
+       customer = Stripe::Customer.create(
+        :email => self.email
+        # :source  => params[:stripeToken]
+      )
+      self.customer_id = customer.id 
+      self.save
+    end
+  end
 end

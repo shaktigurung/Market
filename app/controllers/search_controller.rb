ab05be362fe431080ac84bf34ca82ee910ca44e5
Daniel Teale <djteale90@gmail.com>
2018-10-31 18:41:55 +1100

    Added (ugly) links for search page navigation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,10 @@
 class SearchController < ApplicationController
   def index
-    limit = 2
-    page = params[:page].to_i
-    page ||= 0
-    offset = (page * limit)
-    @search = Product.basic_search(params['search']).limit(limit).offset(offset)
+    @limit = 2
+    @length = Product.basic_search(params['search']).length
+    @page = params[:page].to_i
+    @page ||= 0
+    offset = (@page * @limit)
+    @search = Product.basic_search(params['search']).limit(@limit).offset(offset)
   end
 end

7347cf7ee28d2674a94ebb1ef3d1883fa6319764
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-02 14:55:01 +1100

    Active admin is on

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_003522) do
+ActiveRecord::Schema.define(version: 2018_11_02_033843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
 
   create_table "addresses", force: :cascade do |t|
     t.string "home_number"
@@ -24,6 +38,18 @@ ActiveRecord::Schema.define(version: 2018_11_02_003522) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -73,7 +99,6 @@ ActiveRecord::Schema.define(version: 2018_11_02_003522) do
     t.integer "condition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "category"
     t.index ["category"], name: "index_products_on_category"
     t.index ["description"], name: "index_products_on_description"
@@ -91,9 +116,9 @@ ActiveRecord::Schema.define(version: 2018_11_02_003522) do
     t.string "first_name"
     t.string "last_name"
     t.string "stripe_customer_id"
+    t.string "customer_id"
     t.text "image"
     t.text "description"
-    t.string "customer_id"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

119fbbb820d05decd64ca989b9fcf97b736f24da
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-05 14:00:40 +1100

    styling progress

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,20 @@ ActiveRecord::Schema.define(version: 2018_11_04_232749) do
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
   create_table "addresses", force: :cascade do |t|
     t.string "home_number"
     t.text "street_name"
@@ -26,6 +40,18 @@ ActiveRecord::Schema.define(version: 2018_11_04_232749) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -77,11 +103,12 @@ ActiveRecord::Schema.define(version: 2018_11_04_232749) do
     t.integer "condition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.integer "category"
     t.index ["category"], name: "index_products_on_category"
     t.index ["description"], name: "index_products_on_description"
     t.index ["name"], name: "index_products_on_name"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -95,6 +122,7 @@ ActiveRecord::Schema.define(version: 2018_11_04_232749) do
     t.string "first_name"
     t.string "last_name"
     t.string "stripe_customer_id"
+    t.string "customer_id"
     t.text "image"
     t.text "description"
     t.string "user_name"
@@ -110,4 +138,5 @@ ActiveRecord::Schema.define(version: 2018_11_04_232749) do
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "product_images", "products"
+  add_foreign_key "products", "users"
 end
